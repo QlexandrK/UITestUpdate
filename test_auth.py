6379b729fee4a92891db9815aae5df2db929949c
Qlexandr <lxndrkorobov@gmail.com>
2025-03-28 11:48:19 +0200

    first commit

---
+++ test_auth.py
@@ -0,0 +1,18 @@
+
+
+
+# class Auth:
+#     USERNAME_INPUT = "[data-test = 'username']"
+#     PASSWORD_INPUT = "[data-test='password']"
+#     LOGIN_BTN = "[data-test='login-button']"
+
+
+import pytest
+from pages.main_page import Main
+
+
+@pytest.mark.smoke
+class TestLogin:
+    def test_user_login(self, browser):
+        m = Main(browser)
+        m.user_login()
